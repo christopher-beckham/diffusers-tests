285f01040ea3839df54dc6f36ceb4e820545d412
Christopher Beckham <christopher.j.beckham@gmail.com>
2024-09-18 20:15:10 +0000

    initial

---
+++ flux_controlnet/test_controlnet_union.py
@@ -0,0 +1,110 @@
+import pytest
+import torch
+from diffusers.utils import load_image
+from diffusers import FluxControlNetPipeline, FluxControlNetModel
+from diffusers import AutoencoderKL
+from functools import partial
+
+@pytest.fixture(scope="session")
+def setup_function():
+
+    print("SETTING UP...")
+
+    control_image = load_image(
+        "https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Union-alpha/resolve/main/images/canny.jpg"
+    ).resize((256,256))
+
+    width, height = control_image.size
+
+    base_model = 'black-forest-labs/FLUX.1-dev'
+    controlnet_model = 'InstantX/FLUX.1-dev-Controlnet-Union'
+
+    controlnet = FluxControlNetModel.from_pretrained(
+        controlnet_model, torch_dtype=torch.bfloat16
+    )
+    vae = AutoencoderKL.from_pretrained(base_model, subfolder="vae", torch_dtype=torch.bfloat16)
+    pipe = FluxControlNetPipeline.from_pretrained(
+        base_model, vae=vae, controlnet=controlnet, torch_dtype=torch.bfloat16
+    )
+    pipe.to("cuda")
+
+    prompt = 'A bohemian-style female travel blogger with sun-kissed skin and messy beach waves.'
+
+    generator = torch.Generator(device="cuda").manual_seed(0)
+
+    print("Constructing pipe...")
+
+    pipe_partial = partial(
+        pipe,
+        prompt=prompt,
+        width=width,
+        height=height,
+        num_inference_steps=1, 
+        guidance_scale=3.5,
+        generator=generator
+    )
+
+    yield {"pipe": pipe_partial, "control_image": control_image}
+
+
+def test_one_control_bs2_single_mode(setup_function):
+    """
+    Internally, the batch size of control_image will be 2, this is because we use
+    num_images_per_prompt==2. This should pass.
+
+    """
+    
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    # We cannot do control_image=[control_image, control_image] yet due to some
+    # batch size tracking bug not of my doing.
+    pipe(control_image=control_image, control_mode=0, num_images_per_prompt=2)
+
+
+# This currently fails: the internal batch size is 2 but this is because we actually
+# pass in two control images with [control_image, control_image, and this fails.
+# def test_two_controls_bs1_single_mode(setup_function):
+#     """Even though we pass two control images, a single control mode should be 
+#     interpreted as applying to both of them. This should PASS.
+#     """
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     pipe(control_image=[control_image, control_image], control_mode=0, num_images_per_prompt=1)
+    
+
+# def test_one_image_none_control(setup_function):
+#     """one control image, and we specify the control mode with None.
+
+#     should FAIL
+#     """
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     with pytest.raises(Exception):
+#         # should raise an exception
+#         pipe(control_image=control_image, control_mode=None)
+
+# def test_one_image_two_control(setup_function):
+#     """One control image, and we specify two control modes (makes no sense).
+
+#     This should fail, since it doesn't make sense to give two controls for the same
+#     control image.
+#     """
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     with pytest.raises(Exception):
+#         # should raise an exception
+#         pipe(control_image=control_image, control_mode=[0,1])
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ flux_controlnet/test_multicontrolnet.py
@@ -0,0 +1,212 @@
+import pytest
+import torch
+from diffusers.utils import load_image
+from diffusers import FluxControlNetPipeline, FluxControlNetModel, FluxMultiControlNetModel
+from diffusers import AutoencoderKL
+from functools import partial
+import numpy as np
+
+def pil_to_numpy(image):
+    """to (c,h,w)"""
+    return (np.array(image).astype(np.float32)/255.).swapaxes(1,2).swapaxes(0,1)
+
+def pil_to_torch(image):
+    return torch.from_numpy(pil_to_numpy(image)).float()
+
+@pytest.fixture(scope="session")
+def setup_function():
+
+    print("SETTING UP...")
+    
+    # NOTE can we meta device this to make it faster???
+
+    control_image = load_image(
+        "https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Union-alpha/resolve/main/images/canny.jpg"
+    ).resize((512,512))
+
+    width, height = control_image.size
+
+    base_model = 'black-forest-labs/FLUX.1-dev'
+    controlnet_union = FluxControlNetModel.from_pretrained(
+        'InstantX/FLUX.1-dev-Controlnet-Union', torch_dtype=torch.bfloat16
+    )
+    controlnet_depth = FluxControlNetModel.from_pretrained(
+        "Shakker-Labs/FLUX.1-dev-ControlNet-Depth", torch_dtype=torch.bfloat16
+    )
+    
+    multinet = FluxMultiControlNetModel([controlnet_union, controlnet_depth])
+    vae = AutoencoderKL.from_pretrained(base_model, subfolder="vae", torch_dtype=torch.bfloat16)
+    pipe = FluxControlNetPipeline.from_pretrained(
+        base_model, vae=vae, controlnet=multinet, torch_dtype=torch.bfloat16
+    )
+    #pipe.to("cuda")
+    pipe.enable_model_cpu_offload()
+
+    prompt = 'A bohemian-style female travel blogger with sun-kissed skin and messy beach waves.'
+
+    generator = torch.Generator(device="cuda").manual_seed(0)
+
+    print("Constructing pipe...")
+
+    pipe_partial = partial(
+        pipe,
+        prompt=prompt,
+        width=width,
+        height=height,
+        num_inference_steps=1, 
+        guidance_scale=3.5,
+        generator=generator
+    )
+
+    yield {"pipe": pipe_partial, "control_image": control_image}
+
+
+def test_torch_tensor_multinet_control_modes(setup_function):
+    """
+    This should pass. We pass in two control images but we also
+    pass in two modes, so no problem.
+    """
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    # a batch size of 4
+    control_image = pil_to_torch(control_image).unsqueeze(0)
+
+    pipe(
+        # the controlnets here are [union, union]
+        control_image=[control_image], 
+        control_mode=[0], 
+        controlnet_conditioning_scale=[1.]
+    )
+
+
+def test_multinet_control_modes(setup_function):
+    """
+    This should pass. We pass in two control images but we also
+    pass in two modes, so no problem.
+    """
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    pipe(
+        # the controlnets here are [union, union]
+        control_image=[control_image, control_image], 
+        control_mode=[0, None], 
+        controlnet_conditioning_scale=[1., 1.]
+    )
+
+def test_multinet_control_modes_3(setup_function):
+    """
+    This should fail. We pass in an integer for control mode.
+    """
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    with pytest.raises(Exception):
+        pipe(
+            # the controlnets here are [union, union]
+            control_image=[control_image], 
+            control_mode=0, 
+            controlnet_conditioning_scale=[1.]
+        )
+
+def test_multinet_control_modes_4(setup_function):
+    """
+    This should fail. We pass in more control modes than control images.
+    """
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    with pytest.raises(Exception):
+        pipe(
+            # the controlnets here are [union, union]
+            control_image=[control_image], 
+            control_mode=[0,0], 
+            controlnet_conditioning_scale=[1.]
+        )
+
+def test_multinet_control_modes_2(setup_function):
+    """
+    This should pass. We pass in one control image and one
+    control mode.
+    """
+    obj = setup_function
+    pipe = obj["pipe"]
+    control_image = obj["control_image"]
+
+    pipe(
+        # the controlnets here are [union, union]
+        control_image=[control_image], 
+        control_mode=[0], 
+        controlnet_conditioning_scale=[1.]
+    )
+
+
+
+# def test_multinet_control_modes(setup_function):
+#     """The old branch would have converted [None,None] to [-1, -1] and then it would fail
+#     due to a device-side assert error. On my code this should pass.
+#     """
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     # for the list [1,2,None,None]
+#     # [1,2] implies it's for a union controlnet
+#     # [None,None] implies it's for a regular controlnet,
+#     # these will be converted to -1 but it should not trip
+#     # the controlnet because self.union check
+
+#     # I do not want any exceptions to occur here.
+#     #with pytest.raises(Exception):
+#     pipe(
+#         # the controlnets here are [union, union]
+#         control_image=[control_image, control_image], 
+#         control_mode=[0, None], 
+#         controlnet_conditioning_scale=[1., 1.]
+#     )
+
+# def test_multi_control_modes_wrong_ordering(setup_function):
+#     """Test a mixture of control modes.
+
+#     This *should fail* cos we got the control modes the wrong way around.
+#     """
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     with pytest.raises(Exception):
+#         pipe(
+#             control_image=[control_image, control_image], 
+#             control_mode=[None, 0], 
+#             controlnet_conditioning_scale=[1., 1.]
+#         )
+
+
+# def test_baseline(setup_function):
+#     """Test a mixture of control modes."""
+    
+#     obj = setup_function
+#     pipe = obj["pipe"]
+#     control_image = obj["control_image"]
+
+#     # The current code will convert Nones to -1 but this doesn't make sense and will
+#     # result in an embedding layer being indexed into with a -ve value. So it should
+#     # fail.
+#     pipe(
+#         control_image=[control_image, control_image], 
+#         control_mode=[0,1], 
+#         controlnet_conditioning_scale=[1., 1.]
+#     )
+
+#     assert True
+
+
+if __name__ == '__main__':
+    pytest.main()
